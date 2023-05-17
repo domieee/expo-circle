dfee8c5c928c0ed79285a8e581269a551f9992ac
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-05-17 16:29:08 +0200

    feat: added like function

--- Screens/components/Post.js
+++ Screens/components/Post.js
@@ -1,44 +1,45 @@
-import { View, Text, Image, StyleSheet, TouchableOpacity, Pressable, } from 'react-native';
-import React,{useState} from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Pressable } from 'react-native';
+import React, { useState } from 'react';
 
 const Post = ({ profileImage, postImage, timestamp, userName, jobTitle, hashtags, likes, comments, navigation, postCaption, id }) => {
-    console.log(postCaption)
-    console.log(hashtags)
-    console.log(timestamp)
+    console.log(postCaption);
+    console.log(hashtags);
+    console.log(timestamp);
 
-    const [likeToggle, setLikeToggle] = useState(false)
+    const [likeToggle, setLikeToggle] = useState(false);
 
-    const liked = require('../../assets/img/heart_filled.png')
-    const notLiked = require('../../assets/img/heart_outlined.png')
+    const liked = require('../../assets/img/heart_filled.png');
+    const notLiked = require('../../assets/img/heart_outlined.png');
 
-    const newLikeHigher = likes + 1 
-  
+    const newLikeHigher = likes + 1;
+
     const likeHandler = () => {
-        setLikeToggle((prev) => !prev)
-    }
+        setLikeToggle((prev) => !prev);
+    };
 
     return (
         <>
             <View style={styles.container}>
                 <Pressable style={styles.row} onPress={() => navigation.navigate('ProfileMember', { userIdParameter: userName })}>
                     <Image style={styles.avatar} source={{ uri: profileImage }} />
-    return (
-        <>
-            <View style={styles.container}>
-                <Pressable
-                    onPress={() => { navigation.navigate('ProfileMember', { userIdParameter: userName }) }}
-                    style={styles.row}>
-                    < Image style={styles.avatar} source={{ uri: profileImage }} />
+                    <View style={styles.textLineContainer}>
+                        <Text style={styles.textBold}>{userName}</Text>
+                        <Text style={styles.text}>{jobTitle}</Text>
+                    </View>
+                </Pressable>
 
+                <Image style={styles.imagePost} source={{ uri: postImage }} />
+                <View style={styles.postStats}>
+                    <View style={styles.touchableOpacity}>
+                        <TouchableOpacity onPress={likeHandler} style={styles.stats}>
+                            <Image style={styles.vector} source={likeToggle ? liked : notLiked} />
+                        </TouchableOpacity>
+
+                        <Text style={styles.statsText}>{likeToggle === false ? likes : newLikeHigher}</Text>
+                    </View>
+                    <View style={styles.touchableOpacity}>
                         <TouchableOpacity style={styles.stats} onPress={() => navigation.navigate('Comments', { id: id })}>
                             <Image style={styles.vector} source={require('../../assets/img/comment_outlined.png')} />
-
-                        <TouchableOpacity
-                            style={styles.stats}
-                            onPress={() => navigation.navigate('Comments', { id: id, navigation: navigation })}>
-                            <Image
-                                style={styles.vector}
-                                source={require('../../assets/img/comment_outlined.png')} />
                         </TouchableOpacity>
                         <Text style={styles.statsText}>{comments}</Text>
                     </View>
@@ -75,18 +76,18 @@ const styles = StyleSheet.create({
     row: {
         flexDirection: 'row',
         marginBottom: 10,
-        marginLeft: 15
+        marginLeft: 15,
     },
     textLineContainer: {
         justifyContent: 'center',
-        gap: 2.5
+        gap: 2.5,
     },
     textLine: {
         width: 250,
         height: 20,
         backgroundColor: '#E0E0E0',
         marginRight: 10,
-        borderRadius: 7.5
+        borderRadius: 7.5,
     },
     imagePost: {
         flex: 1,
@@ -94,22 +95,22 @@ const styles = StyleSheet.create({
         width: 350,
         borderRadius: 20,
         marginLeft: 'auto',
-        marginRight: 'auto'
+        marginRight: 'auto',
     },
     postStats: {
         flexDirection: 'row',
         gap: 7.5,
         marginLeft: 15,
-        marginTop: 10
+        marginTop: 10,
     },
     vector: {
         width: 26,
-        height: 26
+        height: 26,
     },
     touchableOpacity: {
         flexDirection: 'row',
         alignItems: 'center',
-        marginRight: 5
+        marginRight: 5,
     },
     hashtags: {
         marginLeft: 15,
@@ -121,28 +122,27 @@ const styles = StyleSheet.create({
     hashtag: {
         color: 'navy',
         fontWeight: 'bold',
-        fontSize: 12
+        fontSize: 12,
     },
     stats: {
         justifyContent: 'center',
     },
     statsText: {
-        marginLeft: 3
+        marginLeft: 3,
     },
     text: {
         marginLeft: 15,
-
     },
     textBold: {
         fontWeight: '600',
-        marginLeft: 15
+        marginLeft: 15,
     },
     caption: {
         marginTop: 10,
         marginBottom: 2.5,
         marginLeft: 15,
-        fontSize: 14
-    }
+        fontSize: 14,
+    },
 });
 
 export default Post;
