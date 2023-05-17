a6b224c8363ec8572e9ddf5fa4e254928d88eb22
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-05-17 15:40:51 +0200

    feat: added like function

--- Screens/components/Post.js
+++ Screens/components/Post.js
@@ -1,50 +1,57 @@
 import { View, Text, Image, StyleSheet, TouchableOpacity, Pressable, } from 'react-native';
-import React from 'react';
+import React,{useState} from 'react';
 
 const Post = ({ profileImage, postImage, timestamp, userName, jobTitle, hashtags, likes, comments, navigation, postCaption, id }) => {
     console.log(postCaption)
     console.log(hashtags)
     console.log(timestamp)
+
+
+    const [likeToggle, setLikeToggle] = useState(false)
+
+    const liked = require('../../assets/img/heart_filled.png')
+    const notLiked = require('../../assets/img/heart_outlined.png')
+
+    const newLikeHigher = likes + 1 
+  
+    const likeHandler = () => {
+        setLikeToggle((prev) => !prev)
+    }
+
     return (
         <>
             <View style={styles.container}>
-                <Pressable
-                    style={styles.row}
-                    onPress={() => navigation.navigate('ProfileMember', { userIdParameter: userName })} >
-                    < Image style={styles.avatar} source={{ uri: profileImage }} />
+                <Pressable style={styles.row} onPress={() => navigation.navigate('ProfileMember', { userIdParameter: userName })}>
+                    <Image style={styles.avatar} source={{ uri: profileImage }} />
                     <View style={styles.textLineContainer}>
                         <Text style={styles.textBold}>{userName}</Text>
                         <Text style={styles.text}>{jobTitle}</Text>
                     </View>
-                </Pressable >
+                </Pressable>
 
                 <Image style={styles.imagePost} source={{ uri: postImage }} />
                 <View style={styles.postStats}>
                     <View style={styles.touchableOpacity}>
-                        <TouchableOpacity style={styles.stats}>
-                            <Image
-                                style={styles.vector}
-                                source={require('../../assets/img/heart_outlined.png')}
-                            />
+                        <TouchableOpacity onPress={likeHandler} style={styles.stats}>
+                            <Image style={styles.vector} source={likeToggle ? liked : notLiked} />
                         </TouchableOpacity>
-                        <Text style={styles.statsText}>{likes}</Text>
+
+                        <Text style={styles.statsText}>{likeToggle === false ? likes : newLikeHigher}</Text>
                     </View>
                     <View style={styles.touchableOpacity}>
-                        <TouchableOpacity
-                            style={styles.stats}
-                            onPress={() => navigation.navigate('Comments', { id: id })}>
-                            <Image
-                                style={styles.vector}
-                                source={require('../../assets/img/comment_outlined.png')} />
+                        <TouchableOpacity style={styles.stats} onPress={() => navigation.navigate('Comments', { id: id })}>
+                            <Image style={styles.vector} source={require('../../assets/img/comment_outlined.png')} />
                         </TouchableOpacity>
                         <Text style={styles.statsText}>{comments}</Text>
                     </View>
                 </View>
                 <Text style={styles.caption}>{postCaption}</Text>
                 <View style={styles.hashtags}>
-                    {hashtags.map((hashtag) => <Text style={styles.hashtag}>{hashtag}</Text>)}
+                    {hashtags.map((hashtag) => (
+                        <Text style={styles.hashtag}>{hashtag}</Text>
+                    ))}
                 </View>
-            </View >
+            </View>
         </>
     );
 };
