297138807ac7a8cd13d204b973a5386bebd36332
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-05-17 19:20:33 +0200

    feat: added FollowButton component :rocket:

--- Screens/components/FollowButton.js
+++ Screens/components/FollowButton.js
@@ -11,7 +11,7 @@ const FollowButton = ({ fullName, image }) => {
                 <Image style={styles.image} source={{ uri: image }} />
             </View>
             <Pressable onPress={() => setRenderState((prev) => !prev)} style={renderState ? styles.buttonClicked : styles.button}>
-                <Text style={styles.text}>{renderState ? 'Unfollow' : 'Follow'}</Text>
+                <Text style={renderState ? styles.textClicked : styles.text}>{renderState ? 'Unfollow' : 'Follow'}</Text>
             </Pressable>
         </View>
     );
@@ -21,60 +21,68 @@ export default FollowButton;
 
 const styles = StyleSheet.create({
     searchedUserContainer: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        textAlign:"center",
-        alignItems:"center",
-        backgroundColor:"#efefef",
-        marginTop:10,
-        marginBottom:10,
-        paddingLeft:10,
-        borderRadius:10,
-        borderWidth:1,
-        borderColor:"lightgray",
-        marginLeft:10,
-        marginRight:10,
-        paddingTop:5,
-        paddingBottom:5
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        textAlign: 'center',
+        alignItems: 'center',
+        backgroundColor: '#efefef',
+        marginTop: 10,
+        marginBottom: 10,
+        paddingLeft: 10,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: 'lightgray',
+        marginLeft: 10,
+        marginRight: 10,
+        paddingTop: 5,
+        paddingBottom: 5,
     },
-    nameContainer:{
-        flexDirection:"row",
-        justifyContent:"space-between",
-        width:210,
-        alignItems:"center"
+    nameContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: 210,
+        alignItems: 'center',
     },
-    image:{
-        width:50,
-        height:50,
-        borderRadius:100
+    image: {
+        width: 50,
+        height: 50,
+        borderRadius: 100,
     },
-    textName:{
-        textAlign:"center",
-        fontSize:15
+    textName: {
+        fontSize: 15,
     },
-    text:{
-        textAlign:"center",
-        fontSize:15,
-        color:"white",
-        fontWeight:"bold",
-        width:70
+    text: {
+        color: 'white',
+        fontWeight: 'bold',
+        width: 70,
+        marginLeft: "auto",
+        marginRight:"auto"
     },
-    button:{
-        borderRadius:10,
-        borderWidth:1,
-        width:120,
-        padding:5,
-        margin:5,
-        borderColor:"lightgray",
-        backgroundColor:"#e98090"
+    textClicked: {
+        textAlign: 'center',
+        color: '#e98090',
+        fontWeight: 'bold',
+        width: 70,
+        marginLeft: "auto",
+        marginRight:"auto"
     },
-    buttonClicked:{
-        borderRadius:10,
-        borderWidth:1,
-        width:120,
-        padding:5,
-        margin:5,
-        backgroundColor:"red",
-        borderColor:"lightgray"
-    }
-})
+    button: {
+        borderRadius: 10,
+        borderWidth: 1,
+        width: 120,
+        padding: 5,
+        margin: 5,
+        borderColor: 'lightgray',
+        backgroundColor: '#e98090',
+        textAlign: 'center',
+    },
+    buttonClicked: {
+        borderRadius: 10,
+        borderWidth: 1,
+        width: 120,
+        padding: 5,
+        margin: 5,
+        borderColor: '#e98090',
+        textAlign: 'center',
+    },
+});
